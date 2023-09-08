be03f31575f4855e406efcdd2a2845b59d431d71
hsseor <hsseor@naver.com>
2023-09-08 17:32:30 +0900

    initial setting

---
+++ MVVMPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMPractice
+//
+//  Created by 홍서린 on 2023/09/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
